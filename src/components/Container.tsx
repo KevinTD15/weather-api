b80c4b8c2ffdde7e6a3cad33aba0f2a90bd36478
KevinTD15 <talaveradiazkevin1999@gmail.com>
2024-07-12 11:54:13 -0500

    ccontainer created

---
+++ src/components/Container.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+function Container({}: Props) {
+  return (
+    <div className="w-full bg-white border rounded-xl flex py-4 shadow-sm">
+      Container
+    </div>
+  );
+}
+
+export default Container;
