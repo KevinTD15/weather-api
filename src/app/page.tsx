82403bdfeb0e3a0b643b625382f6878438d54985
KevinTD15 <talaveradiazkevin1999@gmail.com>
2024-07-12 21:23:06 -0500

    added loading page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,12 +106,7 @@ export default function Home() {
     });
   });
 
-  if (isPending)
-    return (
-      <div className="flex items-center min-h-screen justify-center">
-        <p className="animate-bounce">Loading...</p>
-      </div>
-    );
+  if (isPending) return <Loading />;
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
