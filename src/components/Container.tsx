50be3cbf0a31afbb40ce9811b71e1805122aa303
KevinTD15 <talaveradiazkevin1999@gmail.com>
2024-07-12 21:20:33 -0500

    container created

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,17 @@
+import { cn } from "@/utils/cn";
 import React from "react";
 
 type Props = {};
 
-function Container({}: Props) {
+function Container(props: React.HTMLProps<HTMLDivElement>) {
   return (
-    <div className="w-full bg-white border rounded-xl flex py-4 shadow-sm">
-      Container
-    </div>
+    <div
+      {...props}
+      className={cn(
+        "w-full bg-white border rounded-xl flex py-4 shadow-sm",
+        props.className
+      )}
+    />
   );
 }
 
